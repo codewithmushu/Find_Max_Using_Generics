07f2f2b89f1a6a8f3a4d5ced9fb12510a7e0b910
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-03-28 20:57:37 +0530

    [Muskan] Added : Csharp program to calculate maximum number from 3 numbers.

---
+++ FindMaximum.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class FindMaximum<T> where T : IComparable
+    {
+        public static T MaximumIntegertNumber(T firstValue, T secondValue, T thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue)>0 && firstValue.CompareTo(thirdValue)>0 ||
+               firstValue.CompareTo(secondValue)>=0 && firstValue.CompareTo(thirdValue)>0 ||
+               firstValue.CompareTo(secondValue)>0 && firstValue.CompareTo(thirdValue) >= 0)
+            {
+                return firstValue;
+            }  
+
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
+               firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
+               firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
+            {
+                return secondValue;
+            }
+
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
+               firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
+               firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
+            {
+                return thirdValue;
+            }
+
+            return firstValue;
+        }
+    }
+}
